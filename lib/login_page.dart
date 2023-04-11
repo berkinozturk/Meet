8ff3288a8a699dea164fe2b16b6d692eef3e95bc
Berkin Öztürk <berkin.ozturk@std.izmirekonomi.edu.tr>
2023-04-12 02:32:04 +0300

    First Commit

---
+++ lib/login_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
